0babfa830c1df8fcf06ba4bec35235ee1ebc1838
kelovac <dadokelovac@gmail.com>
2021-05-19 14:03:11 +0200

    Mala ispravka

--- 03-back-end/src/components/article/controller.ts
+++ 03-back-end/src/components/article/controller.ts
@@ -123,17 +123,20 @@ class ArticleController extends BaseController {
             res.sendStatus(400);
             return;
         }
-        
-        const data = JSON.parse(req.body?.data);
+        try {
+            const data = JSON.parse(req.body?.data);
 
-        if (!IAddArticleValidator(data)) {
-            res.status(400).send(IAddArticleValidator.errors);
-            return;
+            if (!IAddArticleValidator(data)) {
+                res.status(400).send(IAddArticleValidator.errors);
+                return;
+            }
+
+            const result = await this.services.articleService.add(data as IAddArticle, uploadedPhotos);
+
+            res.send(result);
+        } catch (e) {
+            res.status(400).send(e?.message);
         }
-
-        const result = await this.services.articleService.add(data as IAddArticle, uploadedPhotos);
-
-        res.send(result);
     }
 }
 
