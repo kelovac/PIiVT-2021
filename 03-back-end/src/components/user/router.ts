dfda839f9da37979e8c467db9becbb83b0c870a6
kelovac <dadokelovac@gmail.com>
2021-05-21 15:50:39 +0200

    Create an action to register a new user account and send a registration confirmation email

--- 03-back-end/src/components/user/router.ts
+++ 03-back-end/src/components/user/router.ts
@@ -7,10 +7,11 @@ export default class UserRouter implements IRouter {
     public setupRoutes(application: Application, resources: IApplicationResources) {
         const userController = new UserController(resources);
 
-        application.get("/user",         userController.getAll.bind(userController));
-        application.get("/user/:id",     userController.getById.bind(userController));
-        application.post("/user",        userController.add.bind(userController));
-        application.put("/user/:id",     userController.edit.bind(userController));
-        application.delete("/user/:id",  userController.delete.bind(userController));
+        application.get("/user",                 userController.getAll.bind(userController));
+        application.get("/user/:id",             userController.getById.bind(userController));
+        application.post("/user",                userController.add.bind(userController));
+        application.put("/user/:id",             userController.edit.bind(userController));
+        application.delete("/user/:id",          userController.delete.bind(userController));
+        application.post("/auth/user/register",  userController.register.bind(userController));
     }
 }
