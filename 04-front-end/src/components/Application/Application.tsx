16d15a542daf61c935fd800d3a19bc8ea7e1f40a
kelovac <dadokelovac@gmail.com>
2021-05-26 14:01:37 +0200

    Create a few components and route them in the application's page body

--- 04-front-end/src/components/Application/Application.tsx
+++ 04-front-end/src/components/Application/Application.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CategoryPage from '../CategoryPage/CategoryPage';
+import HomePage from '../HomePage/HomePage';
+import TopMenu from '../TopMenu/TopMenu';
 import './Application.sass';
 
 export default function Application() {
   return (
-    <div className="Application container">
-      <div className="Application-header">
-        Front-end aplikacije
-      </div>
-      <p>
-        <button className="btn btn-primary">
-          Log in
-        </button>
-      </p>
-    </div>
+    <BrowserRouter>
+      <Container className="Application">
+        <div className="Application-header">
+          Front-end aplikacije
+        </div>
+        <TopMenu />
+
+        <div className="Application-body">
+          <Switch>
+            <Route exact path="/" component={ HomePage } />
+
+            <Route path="/category" component={ CategoryPage } />
+
+            <Route path="/contact">
+              Contact information
+            </Route>
+            
+            <Route path="/profile">
+              My profile
+            </Route>
+          </Switch>
+        </div>
+
+        <div>
+          &copy; 2021...
+        </div>
+      </Container>
+    </BrowserRouter>
   );
 }
 
