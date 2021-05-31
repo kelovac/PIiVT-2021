a86acfdb2e79ea2f4fc6346f26da9f9bc41bb1ab
kelovac <dadokelovac@gmail.com>
2021-05-31 16:24:17 +0200

    Create a category list dashboard page

--- 04-front-end/src/services/CategoryService.ts
+++ 04-front-end/src/services/CategoryService.ts
@@ -1,16 +1,17 @@
 import CategoryModel from '../../../03-back-end/src/components/category/model';
 import api from '../api/api';
 import EventRegister from '../api/EventRegister';
+import { ApiRole } from '../api/api';
 
 export default class CategoryService {
-    public static getTopLevelCategories(): Promise<CategoryModel[]> {
+    public static getTopLevelCategories(role: ApiRole = "user"): Promise<CategoryModel[]> {
         return new Promise<CategoryModel[]>(resolve => {
-            api("get", "/category", "user")
+            api("get", "/category", role)
             .then(res => {
                 if (res?.status !== "ok") {
-                    if (res?.status === "login") {
+                    if (res.status === "login") {
                         EventRegister.emit("AUTH_EVENT", "force_login");
-                    }                   
+                    }
                     return resolve([]);
                 }
 
@@ -19,12 +20,12 @@ export default class CategoryService {
         });
     }
 
-    public static getCategoryById(categoryId: number): Promise<CategoryModel|null> {
+    public static getCategoryById(categoryId: number, role: ApiRole = "user"): Promise<CategoryModel|null> {
         return new Promise<CategoryModel|null>(resolve => {
-            api("get", "/category/" + categoryId, "user")
+            api("get", "/category/" + categoryId, role)
             .then(res => {
                 if (res?.status !== "ok") {
-                    if (res?.status === "login") {
+                    if (res.status === "login") {
                         EventRegister.emit("AUTH_EVENT", "force_login");
                     }
                     return resolve(null);
